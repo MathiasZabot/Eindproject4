96ee94d9436256d00e70850bb7c21b3d99471f19
Frederik Wiame <wiame.frederik@gmail.com>
2019-06-14 22:01:26 +0200

    final touch

--- mvc/views/updateBedrijf/index.php
+++ mvc/views/updateBedrijf/index.php
@@ -1,9 +1,8 @@
 <?php include("views/templates/header.php");?>
     <form method="post" action="index.php?page=update_done_bedrijf">
-        <input type="hidden" value="<?php echo $_GET['id']; ?>" name="bedrijf_id">
+       
+        <input type="text" class="form-control" id="exampleFormControlInput1" name="naam" >
 
-        <input type="text" class="form-control" id="exampleFormControlInput1" name="naam" value="<?php echo($result[0]['naam'])?>">
-
-        <button type="submit" class="btn btn-primary">Aanpassen</button>
+        <button type="submit" class="btn btn-primary">Toevoegen</button>
     </form>
 <?php include("views/templates/footer.php");?>
